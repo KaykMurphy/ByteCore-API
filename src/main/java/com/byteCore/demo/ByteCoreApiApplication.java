e8d0e0cafacedc5108bc2727d21989a23ea75c9e
Kayk el <kayksantana133@gmail.com>
2026-01-28 19:41:56 -0300

    initial commit: project setup with spring initializr

---
+++ src/main/java/com/byteCore/demo/ByteCoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.byteCore.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteCoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByteCoreApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/byteCore/demo/ByteCoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byteCore.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByteCoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
