4cf14d26659aaf0643e27c2a5f88cbdf95f54ed7
Kayk el <kayksantana133@gmail.com>
2026-01-28 20:49:37 -0300

    feat: add ProductRepository with custom query methods

---
+++ src/main/java/com/byteCore/demo/repository/ProductRepository.java
@@ -0,0 +1,17 @@
+package com.byteCore.demo.repository;
+
+import com.byteCore.demo.domain.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Page<Product> findByActiveTrue(Pageable pageable);
+    List<Product> findByTitleContainingIgnoreCase(String title);
+
+}
