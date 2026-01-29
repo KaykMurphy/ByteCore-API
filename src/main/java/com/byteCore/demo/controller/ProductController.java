c204d22d0dfc1d4bea5900340a7b6f4b77e272f2
Kayk el <kayksantana133@gmail.com>
2026-01-29 19:56:55 -0300

    feat: add ProductController with REST endpoints for product queries

---
+++ src/main/java/com/byteCore/demo/controller/ProductController.java
@@ -0,0 +1,42 @@
+package com.byteCore.demo.controller;
+
+import com.byteCore.demo.dto.response.ProductResponseDTO;
+import com.byteCore.demo.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+
+    @GetMapping
+    public ResponseEntity<Page<ProductResponseDTO>> getAllProducts(Pageable pageable) {
+        Page<ProductResponseDTO> response = productService.findlAll(pageable);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponseDTO> getProductById(@PathVariable Long id){
+        ProductResponseDTO response = productService.findById(id);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<ProductResponseDTO>> searchProducts(
+            @RequestParam String query){
+        List<ProductResponseDTO> response = productService.searchByTitle(query);
+
+        return ResponseEntity.ok(response);
+    }
+
+}
