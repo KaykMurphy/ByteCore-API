03659fe566f1c91f5969f4384e4a7ff8dfbed066
Kayk el <kayksantana133@gmail.com>
2026-02-04 18:16:01 -0300

    feat: add custom UserDetails, JWT utils and authentication filter

---
+++ src/main/java/com/byteCore/demo/security/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.byteCore.demo.security;
+
+import com.byteCore.demo.domain.User;
+import com.byteCore.demo.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado: "+email));
+
+        return new CustomUserDetails(user);
+    }
+}
+
