7a5b9b57758a5236f70e6ed0d21022021d6aee73
kayk <kayksantana133@gmail.com>
2026-04-09 18:51:09 -0300

    fix: move ByteCoreApiApplication back to root package

--- src/main/java/com/byteCore/demo/ByteCoreApiApplication.java
+++ src/main/java/com/byteCore/demo/ByteCoreApiApplication.java
@@ -1,4 +1,4 @@
-package com.byteCore.demo.service;
+package com.byteCore.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
